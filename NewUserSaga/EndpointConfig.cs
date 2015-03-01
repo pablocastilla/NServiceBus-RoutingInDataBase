533122174bf7a780e9bbecebcd519dacc82d76c5
pablocastilla <pablo.castilla@gmail.com>
2015-03-01 08:11:03 +0100

    I left a few things behind :)

---
+++ NewUserSaga/EndpointConfig.cs
@@ -0,0 +1,22 @@
+
+namespace NewUserSaga
+{
+    using DynamicRouting;
+    using NServiceBus;
+    using NServiceBus.Features;
+
+    /*
+		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
+		can be found here: http://particular.net/articles/the-nservicebus-host
+	*/
+    public class EndpointConfig : IConfigureThisEndpoint
+    {
+        public void Customize(BusConfiguration configuration)
+        {
+            configuration.UsePersistence<DynamicRoutingInMemoryPersistence>();
+            configuration.DisableFeature<AutoSubscribe>();
+            configuration.Pipeline.Register<DynamicRouting.DynamicRoutingStepInPipeline>();
+            configuration.CustomConfigurationSource(new DynamicRoutingConfiguration());
+        }
+    }
+}
---
+++ NewUserSaga/NewUserSaga.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messages;
+using NServiceBus;
+using NServiceBus.Saga;
+
+namespace NewUserSaga
+{
+    public class NewUserSaga : Saga<NewUserSagaData>, IAmStartedByMessages<NewUser>, IHandleMessages<CreateUserResponse>
+    {
+        
+        public void Handle(NewUser message)
+        {
+            this.Data.Name = message.Name;
+
+            Console.WriteLine("New User Saga started for "+this.Data.Name);
+            Bus.Send(new CreateUser() { Name=message.Name});
+        }
+
+        public void Handle(CreateUserResponse message)
+        {
+            Console.WriteLine("reply received from createuserhandler " + this.Data.Name);
+
+            this.MarkAsComplete();
+        }
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<NewUserSagaData> mapper)
+        {
+           
+        }
+    }
+
+    public class NewUserSagaData : ContainSagaData
+    {
+        [Unique]
+        public string Name { get; set; }
+    }
+}
